06741f8edaccad85e1eb493c161297598c5b541f
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2021-01-04 13:51:39 +0100

    Add Teamcity support for tests reporter
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/json_cppdict/main.cpp
+++ tests/json_cppdict/main.cpp
@@ -1,8 +1,12 @@
 #define CATCH_CONFIG_MAIN
 #if __has_include(<catch2/catch.hpp>)
 #include <catch2/catch.hpp>
+#include <catch2/catch_reporter_teamcity.hpp>
+#include <catch2/catch_reporter_tap.hpp>
 #else
 #include <catch.hpp>
+#include <catch_reporter_teamcity.hpp>
+#include <catch_reporter_tap.hpp>
 #endif
 #include <cstdint>
 #include "config/Config.hpp"
